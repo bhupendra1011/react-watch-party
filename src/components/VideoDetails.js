14b033b627e86efe97460ff2098fe519d0898702
Bhupendra <bhupendra1011@gmail.com>
2020-05-08 09:27:32 +0530

    adding video details component

--- src/components/VideoDetails.js
+++ src/components/VideoDetails.js
@@ -1,7 +1,33 @@
 import React from "react";
+import { Paper, Typography } from "@material-ui/core";
 
 function VideoDetails({ video }) {
-  return <h1>VideoDetails</h1>;
+  if (!video) {
+    return <div>Loading... </div>;
+  }
+  const videoSrc = `https://www.youtube.com/embed/${video.id.videoId}`;
+  return (
+    <>
+      <Paper elevation={6}>
+        <iframe
+          frameBorder="0"
+          height="100%"
+          width="100%"
+          title="Video PLayer"
+          src={videoSrc}
+        />
+      </Paper>
+      <Paper elevation={6}>
+        <Typography variant="h4">
+          {video.snippet.title}- {video.snippet.channelTitle}
+        </Typography>
+        <Typography variant="subtitle1">
+          {video.snippet.channelTitle}
+        </Typography>
+        <Typography variant="subtitle2">{video.snippet.description}</Typography>
+      </Paper>
+    </>
+  );
 }
 
 export default VideoDetails;
